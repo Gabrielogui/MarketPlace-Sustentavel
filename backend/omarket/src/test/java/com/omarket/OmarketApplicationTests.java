e4002ede6126fc2bc96844ba7c5db2e10cfbe85f
Gabrielogui <gabrielcsro@gmail.com>
2025-05-14 14:32:01 -0300

    build: Spring initializr - Criação da estrutura do backend
    > Criação do projeto Spring
    > Dependências: Spring Boot Dev Tools, Spring Data JPA, Spring Security, PostgreSQL Driver, Lombok, Spring Web, Validation.

---
+++ backend/omarket/src/test/java/com/omarket/OmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
