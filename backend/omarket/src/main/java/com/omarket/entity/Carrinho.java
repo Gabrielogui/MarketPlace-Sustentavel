f4ec0ed99723345b0cbe1cd8f977f6eb10a2ee2c
luizvn <luizvinicius128@gmail.com>
2025-05-23 13:10:49 -0300

    build: add entity carrinho

---
+++ backend/omarket/src/main/java/com/omarket/entity/Carrinho.java
@@ -0,0 +1,29 @@
+package com.omarket.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "carrinho")
+@Getter
+@Setter
+public class Carrinho {
+    @Id
+    @Column(name = "cliente_id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @MapsId
+    @JoinColumn(name = "cliente_id", nullable = false)
+    private Cliente cliente;
+
+    
+}
--- backend/omarket/src/main/java/com/omarket/entity/Endereco.java
+++ backend/omarket/src/main/java/com/omarket/entity/Endereco.java
@@ -1,9 +1,17 @@
 package com.omarket.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Table(name = "endereco")
+@Getter
+@Setter
 public class Endereco{
 
     @Id
