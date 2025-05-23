dd05403a29ac325964c0df75acaf6c5137aa2408
Gabrielogui <gabrielcsro@gmail.com>
2025-05-23 10:53:37 -0300

    build: Criação da entity Produto
    > Falta realizar os relacionamentos

---
+++ backend/omarket/src/main/java/com/omarket/entity/Produto.java
@@ -0,0 +1,33 @@
+package com.omarket.entity;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "produto")
+@Getter
+@Setter
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String descricao;
+    private int estoque;
+
+    @Column(name="preco", precision = 19, scale = 4, nullable = false)
+    private BigDecimal preco;
+
+    // FAZER RELACIONAMENTO COM CATEGORIA
+    // FAZER RELACIONAMENTO COM FORNECEDOR
+
+}
