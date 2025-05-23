a63f27b5f76b62404b0a12f9403d614aced09f1f
luizvn <luizvinicius128@gmail.com>
2025-05-23 10:24:13 -0300

    build: add entity usuario
    > tabela gerada pelo hibernate no banco de dados através da criação da entity no backend.

---
+++ backend/omarket/src/main/java/com/omarket/entity/Usuario.java
@@ -0,0 +1,28 @@
+package com.omarket.entity;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuario")
+@Getter
+@Setter
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String senha;
+    private String telefone;
+    private int tipoConta; // 0 - vendedor, 1 - comprador
+    private String status; // ativo ou inativo
+
+}
