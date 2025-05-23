133d9e2d1087fb6d6296d3f26813001eda885f9f
Gabrielogui <gabrielcsro@gmail.com>
2025-05-23 12:56:16 -0300

    build: Cirando entity ItemPedido + Embeddedble ItemPedidoId
    > ItemPedidoId possui tanto o pedido como o produto

---
+++ backend/omarket/src/main/java/com/omarket/entity/ItemPedido.java
@@ -0,0 +1,35 @@
+package com.omarket.entity;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+
+public class ItemPedido {
+    // |=======| ATRIBUTOS |=======|
+    @EmbeddedId
+    private ItemPedidoId id = new ItemPedidoId();
+
+    @Column(nullable = false)
+    private Integer quantidade;
+
+    @Column(name="preco_unitario", precision = 19, scale = 4, nullable = false)    
+    private BigDecimal precoUnitario;
+
+    // |=======| GET E SETS DO ID (PRODUTO E PEDIDO) |=======|
+    public Pedido getPedido() {
+        return id.getPedido();
+    }
+
+    public void setPedido(Pedido pedido) {
+        id.setPedido(pedido);
+    }
+
+    public Produto getProduto() {
+        return id.getProduto();
+    }
+
+    public void setProduto(Produto produto) {
+        id.setProduto(produto);
+    }
+}
