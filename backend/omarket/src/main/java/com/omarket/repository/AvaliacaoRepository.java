e1541d771085c97abc55393d036ca292ee679ae8
luizvn <luizvinicius128@gmail.com>
2025-05-25 16:30:46 -0300

    feat: add repository Avaliacao

---
+++ backend/omarket/src/main/java/com/omarket/repository/AvaliacaoRepository.java
@@ -0,0 +1,20 @@
+package com.omarket.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.omarket.entity.Avaliacao;
+import com.omarket.entity.AvaliacaoId;
+import com.omarket.entity.Cliente;
+import com.omarket.entity.Produto;
+
+@Repository
+public interface AvaliacaoRepository extends JpaRepository<Avaliacao, AvaliacaoId> {
+    // Custom query methods can be defined here if needed
+    // For example, to find all evaluations for a specific product:
+    List<Avaliacao> findByIdProduto(Produto produto);
+    List<Avaliacao> findByIdCliente(Cliente cliente);
+
+}
