a3f843a50599c499aecb6606aae9eef0ea5eec0d
Gabrielogui <gabrielcsro@gmail.com>
2025-05-28 08:11:07 -0300

    feat: Criação do controller de categoria

---
+++ backend/omarket/src/main/java/com/omarket/controller/CategoriaController.java
@@ -0,0 +1,13 @@
+package com.omarket.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/categoria")
+@RequiredArgsConstructor
+public class CategoriaController {
+    
+}
