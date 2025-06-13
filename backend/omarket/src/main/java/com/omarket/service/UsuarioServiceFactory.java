e86b8c0134a48406d6fbadddd71fbc123ec56ae4
Gabrielogui <gabrielcsro@gmail.com>
2025-06-13 09:45:04 -0300

    feat: Criando e implementando UsuarioServiceFactory
    > Implementando padrão de projeto FactoryMethod

---
+++ backend/omarket/src/main/java/com/omarket/service/UsuarioServiceFactory.java
@@ -0,0 +1,25 @@
+package com.omarket.service;
+
+import com.omarket.entity.enum_.TipoUsuario;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class UsuarioServiceFactory {
+    private final ClienteService clienteService;
+    private final FornecedorService fornecedorService;
+    private final AdministradorService administradorService;
+
+    public UsuarioService getUsuarioService(TipoUsuario tipoUsuario){
+        switch (tipoUsuario) {
+            case CLIENTE:
+                return clienteService;
+            case FORNECEDOR:
+                return fornecedorService;
+            case ADMINISTRADOR:
+                return administradorService;
+            default:
+                throw new IllegalArgumentException("Tipo de usuário desconhecido: " + tipoUsuario);
+        }
+    }
+}
