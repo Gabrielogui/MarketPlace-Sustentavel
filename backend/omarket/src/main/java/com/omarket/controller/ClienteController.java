a1be6796a5796dab8f55e912a0db7a80264dc2c6
Gabrielogui <gabrielcsro@gmail.com>
2025-06-13 10:18:08 -0300

    feat: Finalizando ClienteController
    > métodos feitos: inativar e editar

--- backend/omarket/src/main/java/com/omarket/controller/ClienteController.java
+++ backend/omarket/src/main/java/com/omarket/controller/ClienteController.java
@@ -5,8 +5,10 @@ import java.net.URI;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,5 +51,22 @@ public class ClienteController {
         UsuarioDTO usuarioDTO = clienteService.buscar(id);
 
         return ResponseEntity.ok(usuarioDTO);
+    }
+
+    // ======= INATIVAR USUÁRIO =======
+    @PatchMapping("/{id}/inativar")
+    public ResponseEntity<UsuarioDTO> inativar(@PathVariable Long id){
+        UsuarioService clienteService = usuarioServiceFactory.getUsuarioService(TipoUsuario.CLIENTE);
+        UsuarioDTO clienteInativado = clienteService.inativar(id);
+
+        return ResponseEntity.ok(clienteInativado);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioDTO> editar(@PathVariable Long id, @RequestBody @Validated UsuarioDTO usuarioDTO){
+        UsuarioService clienteService = usuarioServiceFactory.getUsuarioService(TipoUsuario.CLIENTE);
+        UsuarioDTO clienteEditado = clienteService.editar(id, usuarioDTO);
+
+        return ResponseEntity.ok(clienteEditado);
     }
 }
