7b02bc8229cee1d0351cb40b28b3639398dae907
Gabrielogui <gabrielcsro@gmail.com>
2025-06-17 15:11:52 -0300

    feat: Adicionando imagem no Header

--- frontend/omarket-frontend/src/components/Header.tsx
+++ frontend/omarket-frontend/src/components/Header.tsx
@@ -1,7 +1,16 @@
+
+/* 
+COMPONENTES PRONTOS QUE SERÃO UTILIZADOS NO MENU: 
+ - DROPDOWNMENU = SERÁ USADO NO SÍMBOLO DE USUÁRIO PARA ABRIR AS OPÇÕES
+ -
+*/
+
+import Image from "next/image";
+
 export default function Header () {
     return(
         <header>
-            Menu
+            <Image src={"/logo_no_bg.png"} alt="omarket" width={100} height={100}></Image>
         </header>
     );
 }
