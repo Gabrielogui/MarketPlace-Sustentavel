c0c7cf968179523f04ffd9d06e9873b28a9e58c6
Gabrielogui <gabrielcsro@gmail.com>
2025-06-19 18:11:49 -0300

    feat: Criando e implementando componente Footer
    > Colocando h-screen no tailwind css do page.tsx -> MOMENTÂNEO = Tirar em breve
    > Baixando imagens do linkedin, instagram e github

--- frontend/omarket-frontend/src/app/layout.tsx
+++ frontend/omarket-frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header";
 import "./globals.css";
+import Footer from "@/components/Footer";
 
 
 export default function RootLayout({
@@ -12,6 +13,7 @@ export default function RootLayout({
       <body >
         <Header/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
