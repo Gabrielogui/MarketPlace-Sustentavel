407238c9d335d65679500bcae69b00eb29044c3b
Gabrielogui <gabrielcsro@gmail.com>
2025-06-24 12:27:55 -0300

    style: Estruturando a rota de pagamento do cliente

---
+++ frontend/omarket-frontend/src/app/cliente/pagamento/page.tsx
@@ -0,0 +1,24 @@
+import { Label } from "@/components/ui/label";
+
+export default function Pagamento () {
+    return(
+        <div>
+            {/* PAGAMENTO */}
+            <div>
+                <Label className="text-3xl font-bold">Pagamento</Label> 
+            </div>
+            {/* TABELA COM PRODUTOS PARA O PAGAMENTO + PREÇOS */}
+            <div>
+                
+            </div>
+            {/* INFORMÇÃO DO PREÇO TOTAL OU ALTERAÇÃO DO ENDEREÇO */}
+            <div>
+
+            </div>
+            {/* FINALIZAÇÃO DO PAGAMENTO */}
+            <div>
+
+            </div>
+        </div>
+    );
+}
