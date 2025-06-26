af3a730e0debb971f0be7d56ca23ddee9ae599a7
Gabrielogui <gabrielcsro@gmail.com>
2025-06-26 15:56:53 -0300

    style: Informações Gerais do pedido

--- frontend/omarket-frontend/src/app/cliente/pedidos/page.tsx
+++ frontend/omarket-frontend/src/app/cliente/pedidos/page.tsx
@@ -1,17 +1,30 @@
+import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
+import { Trash2 } from "lucide-react";
 
 export default function MeusPedidos () {
     return(
-        <div>
+        <div className="flex flex-col gap-10">
             {/* TÍTULO DOS MEUS PEDIDOS */}
             <div>
                 <Label className="text-3xl font-bold">Meus Pedidos</Label>
             </div>
             {/* MOSTRANDO O PEDIDO */}
-            <div>
+            <div className="flex flex-col gap-5">
                 {/* INFORMAÇÃO GERAL DO PEDIDO */}
-                <div>
-
+                <div className="flex flex-row justify-between">
+                    <div className="flex flex-row gap-12">
+                        <Label>XX/XX/XXXX</Label>
+                        <Label>R$ XXX,XX</Label>
+                        <Label>Status: Em entrega</Label>
+                        <Label>Id do Pedido: XXX</Label>
+                    </div>
+                    <div>
+                        <Button>
+                            <Trash2></Trash2>
+                            Excluir Pedido
+                        </Button>
+                    </div>
                 </div>
                 {/* TODOS OS PRODUTOS DO PEDIDO */}
                 <div>
