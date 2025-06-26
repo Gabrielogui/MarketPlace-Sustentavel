701c13afc3f609ed81dbaca36d00317e395ba934
Gabrielogui <gabrielcsro@gmail.com>
2025-06-26 15:13:05 -0300

    style: Estruturando os pedidos do cliente 'Meus Pedidos'

---
+++ frontend/omarket-frontend/src/app/cliente/pedidos/page.tsx
@@ -0,0 +1,23 @@
+import { Label } from "@/components/ui/label";
+
+export default function MeusPedidos () {
+    return(
+        <div>
+            {/* TÍTULO DOS MEUS PEDIDOS */}
+            <div>
+                <Label className="text-3xl font-bold">Meus Pedidos</Label>
+            </div>
+            {/* MOSTRANDO O PEDIDO */}
+            <div>
+                {/* INFORMAÇÃO GERAL DO PEDIDO */}
+                <div>
+
+                </div>
+                {/* TODOS OS PRODUTOS DO PEDIDO */}
+                <div>
+
+                </div>
+            </div>
+        </div>
+    );
+}
