ed345ac54a91cf609b212ed88236e3ed4987e017
Gabrielogui <gabrielcsro@gmail.com>
2025-06-27 11:10:22 -0300

    style: Estruturando dashboard do fornecedor

--- frontend/omarket-frontend/src/app/fornecedor/page.tsx
+++ frontend/omarket-frontend/src/app/fornecedor/page.tsx
@@ -1,7 +1,24 @@
+import { Label } from "@/components/ui/label";
+
 export default function FornecedorHome () {
     return(
-        <div>
-            <h1 className="text-4xl">Fornecedor</h1>
+        <div className="flex flex-col gap-10">
+            {/* ESTATÍSTICAS DO FORNECEDOR */}
+            <div>
+                <Label className="text-4xl">Estatísticas</Label>
+                {/* GRÁFICOS */}
+                <div>
+
+                </div>
+            </div>
+            {/* PRINCIPAIS PRODUTOS DO FORNECEDOR */}
+            <div>
+                <Label className="text-4xl">Meus produtos principais</Label>
+                {/* PRODUTOS */}
+                <div>
+
+                </div>
+            </div>
         </div>
     )
 }
