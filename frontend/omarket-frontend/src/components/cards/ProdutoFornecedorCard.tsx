e420589deadeb91ce910bf45dd5cabcc7fe9c988
Gabrielogui <gabrielcsro@gmail.com>
2025-06-28 15:53:04 -0300

    style: Ajeitando hover do ProdutoFornecedorCard + cursor-pointer nos botões

--- frontend/omarket-frontend/src/components/cards/ProdutoFornecedorCard.tsx
+++ frontend/omarket-frontend/src/components/cards/ProdutoFornecedorCard.tsx
@@ -5,17 +5,17 @@ import { PencilLine, Trash2 } from "lucide-react";
 
 export default function ProdutoFornecedorCard () {
     return(
-        <div className="flex flex-row gap-2">
+        <div className="flex flex-row gap-2 p-2 hover:shadow hover:scale-105 transition-all ">
             <Image src={"https://picsum.photos/150/150"} alt="Produto" height={150} width={150}></Image>
             <div className="flex flex-col justify-between">
                 <Label className="text-2xl">Nome Produto</Label>
                 <Label className="text-gray-500">Categoria</Label>
                 <Label className="text-gray-500">Preço</Label>
                 <div className="flex flex-row justify-start gap-4">
-                    <Button variant={"outline"} size={"icon"}>
+                    <Button className="cursor-pointer" variant={"outline"} size={"icon"}>
                         <PencilLine/>
                     </Button>
-                    <Button variant={"outline"} size={"icon"}>
+                    <Button className="cursor-pointer" variant={"outline"} size={"icon"}>
                         <Trash2/>
                     </Button>
                 </div>
