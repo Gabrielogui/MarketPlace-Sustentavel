bda300d4db3175811338d8132049e6535201b88f
Gabrielogui <gabrielcsro@gmail.com>
2025-06-28 15:22:07 -0300

    style: Menu superior do 'Meus Produtos' do fornecedor

---
+++ frontend/omarket-frontend/src/app/fornecedor/produtos/page.tsx
@@ -0,0 +1,29 @@
+import { Label } from "@/components/ui/label";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+
+export default function MeusProdutos () {
+    return(
+        <div className="flex flex-col gap-10">
+            <div className="flex flex-row justify-between">
+                <Label className="text-3xl">Meus Produtos</Label>
+                <div className="flex flex-col items-center">
+                    <p>Classificar por preço</p>
+                    <Select>
+                        <SelectTrigger>
+                            <SelectValue placeholder="Selecione" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectGroup>
+                                <SelectItem value="Crescente">Crescente</SelectItem>
+                                <SelectItem value="Decrescente">Decrescente</SelectItem>
+                            </SelectGroup>
+                        </SelectContent>
+                    </Select>
+                </div>
+            </div>
+            <div >
+                
+            </div>
+        </div>
+    );
+}
