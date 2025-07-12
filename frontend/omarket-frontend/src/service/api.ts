a812c84e7d45686b8c60cdb2e8c7a5f65c470d11
Gabrielogui <gabrielcsro@gmail.com>
2025-07-11 22:33:32 -0300

    chore: Alteração momentânea do URL base para requisição no backend
    > Pegando do outro computador

--- frontend/omarket-frontend/src/service/api.ts
+++ frontend/omarket-frontend/src/service/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080', 
+  baseURL: 'http://10.0.0.163:8080', 
 });
 
 api.interceptors.request.use(config => {
