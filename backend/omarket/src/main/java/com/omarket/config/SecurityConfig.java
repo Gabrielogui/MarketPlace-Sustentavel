96f9cf94fa806fed971bc3d635d30acd7c840b54
luizvn <luizvinicius128@gmail.com>
2025-08-12 19:35:57 -0300

    feat: implementação de cache aside em CategoriaService

--- backend/omarket/src/main/java/com/omarket/config/SecurityConfig.java
+++ backend/omarket/src/main/java/com/omarket/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.omarket;
+package com.omarket.config;
 
 import java.util.Arrays;
 
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
