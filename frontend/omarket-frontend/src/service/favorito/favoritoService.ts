faf82024f6a07cb146a32497b45f4d400dac63f2
Gabrielogui <gabrielcsro@gmail.com>
2025-08-26 12:00:59 -0300

    feat: Criando rotas de favoritos (favoritar, deletar)

--- frontend/omarket-frontend/src/service/favorito/favoritoService.ts
+++ frontend/omarket-frontend/src/service/favorito/favoritoService.ts
@@ -1,7 +1,17 @@
-import { Favorito } from "@/core";
+import { Favorito, FavoritoAdd } from "@/core";
 import api from "../api";
 
 // |=======| GET DA LISTA DE FAVORITOS POR CLIENTE |=======|
 export async function getListaFavoritos(clienteId: number) {
     return api.get<Favorito[]>(`/favorito/${clienteId}`);
+}
+
+// |=======| POST PARA FAVORITAR PRODUTO |=======|
+export async function favoritar(favorito: FavoritoAdd){
+    return api.post<FavoritoAdd>('favorito/favoritar', favorito);
+}
+
+// |=======| DELETE PARA DESFAVORITAR UM PRODUTO |=======|
+export async function desfavoritar(id: number) {
+    return api.delete<string>(`/favorito/${id}`);
 }
